afed2983a1d78c81b03e738516f772298d24ed1f
Mads Zeneli <mads@zeneli.dk>
2017-01-11 19:46:17 +0100

    ...

--- app.js
+++ app.js
@@ -4,34 +4,34 @@ import Foo from './components/foo/foo';
 
 // Provide components
 const Components = [
-	{Class: Foo, selectorName: 'foo'}
+  {Class: Foo, selectorName: 'foo'}
 ];
 
 
 // Setup app
 export default class App {
 
-	constructor() {
-	   this.createComponents();
-		return this;
-	}
+  constructor() {
+    this.createComponents();
+    return this;
+  }
 
 	createComponents() {
-		this.components = {};
-		for (let i = 0; i < Components.length; i++) {
-			let nodes = [];
-			let tags = document.querySelectorAll('[data-component=' + Components[i].selectorName + ']');
-			for (let x = 0; x < tags.length; x++) {
-				nodes.push(new Components[i].Class(tags[x]));
-			}
-			if(nodes.length) this.components[Components[i].selectorName] = nodes;
-		}
-		console.log('modules (app)', this.getComponents());
-	}
+    this.components = {};
+    for (let i = 0; i < Components.length; i++) {
+      let nodes = [];
+      let tags = document.querySelectorAll('[data-component=' + Components[i].selectorName + ']');
+      for (let x = 0; x < tags.length; x++) {
+        nodes.push(new Components[i].Class(tags[x]));
+      }
+      if(nodes.length) this.components[Components[i].selectorName] = nodes;
+    }
+    console.log('modules (app)', this.getComponents());
+  }
 
-	getComponents(selectorName) {
-		return selectorName ? this.components[selectorName] : this.components;
-	}
+  getComponents(selectorName) {
+    return selectorName ? this.components[selectorName] : this.components;
+  }
 
 }
 
