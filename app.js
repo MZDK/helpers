8c05b503076376a50e0967797bdb7a4592956fa2
Mads Zeneli <mads@zeneli.dk>
2017-01-11 12:34:13 +0100

    Inits repo

---
+++ app.js
@@ -0,0 +1,39 @@
+// Import components
+import Foo from './components/foo/foo';
+
+
+// Provide components
+const Components = [
+	{Class: Foo, 		selectorName: 'foo'}
+];
+
+
+// Setup app
+export default class App {
+
+	constructor() {
+		return this;
+	}
+
+	createComponents() {
+		this.components = {};
+		for (let i = 0; i < Components.length; i++) {
+			let nodes = [];
+			let tags = document.querySelectorAll('[data-component=' + Components[i].selectorName + ']');
+			for (let x = 0; x < tags.length; x++) {
+				nodes.push(new Components[i].Class(tags[x]));
+			}
+			if(nodes.length) this.components[Components[i].selectorName] = nodes;
+		}
+		console.log('modules (app)', this.getComponents());
+	}
+
+	getComponents(selectorName) {
+		return selectorName ? this.components[selectorName] : this.components;
+	}
+
+}
+
+
+// Run app
+window.app = new App();
---
+++ components/foo/foo.js
@@ -0,0 +1,13 @@
+export default class Foo {
+
+	constructor(elem) {
+		this.init(elem);
+		return this;
+	}
+
+	init(elem) {
+		const self = this;
+    console.log('Foo', elem);
+	}
+
+}
